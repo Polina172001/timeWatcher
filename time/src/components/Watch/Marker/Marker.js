7e81bb4d2a86c1925e8e6792ef5aa81f58807630
Polina <polina17012001@mail.ru>
2021-11-19 00:33:19 +0300

    version1

---
+++ time/src/components/Watch/Marker/Marker.js
@@ -0,0 +1,12 @@
+import classes from './Marker.module.css';
+
+const Marker = ({className, offset}) => (
+  <span
+    className={classes[className]}
+    style={{
+      transform: `rotate(${offset}deg)`
+    }}/>
+)
+
+
+export default Marker;
---
+++ time/src/components/WorldWatches.js
@@ -0,0 +1,27 @@
+import Watch from "../components/Watch/Watch";
+import PropTypes from "prop-types";
+
+const WorldWatches = ({watches, deleteWatch}) => {
+  return (
+    <div className="row pt-5">
+      <div className="col d-flex flex-wrap">
+        {watches.map(({id, name, timeZone}) => (
+          <Watch
+            key={id}
+            id={id}
+            title={name}
+            timeZone={timeZone}
+            deleteWatch={deleteWatch}
+          />))
+        }
+      </div>
+    </div>
+  );
+};
+
+WorldWatches.propTypes = {
+  watches: PropTypes.array.isRequired,
+  deleteWatch: PropTypes.func.isRequired
+}
+
+export default WorldWatches;
